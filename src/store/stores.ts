92850b7ce2bd86ba832b7a0a217e6ecf47409e36
kecapasen <qshoescraft@gmail.com>
2024-05-26 16:31:18 +0700

    fix(auth): use zustand persist

--- src/store/stores.ts
+++ src/store/stores.ts
@@ -1,15 +1,24 @@
 import { authSchema } from "../types/storeTypes/types";
 import { z } from "zod";
 import { create } from "zustand";
-export const useUser = create((set) => ({
-  user: { fullname: "", email: "", username: "" } as z.infer<typeof authSchema>,
-  setUser: (data: z.infer<typeof authSchema>) =>
-    set({
-      user: {
-        fullname: data.fullname,
-        email: data.email,
-        username: data.username,
-      },
+import { persist } from "zustand/middleware";
+export const useUser = create(
+  persist(
+    (set) => ({
+      user: { fullname: "", email: "", username: "" } as z.infer<
+        typeof authSchema
+      >,
+      setUser: (data: z.infer<typeof authSchema>) =>
+        set({
+          user: {
+            fullname: data.fullname,
+            email: data.email,
+            username: data.username,
+          },
+        }),
+      removeUser: () =>
+        set({ user: { fullname: "", email: "", username: "" } }),
     }),
-  removeUser: () => set({ user: { fullname: "", email: "", username: "" } }),
-}));
+    { name: "authStore" }
+  )
+);
